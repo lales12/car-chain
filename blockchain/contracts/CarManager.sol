6d7e6bfa204b038c3cb5ac39764f049bf7181574
Rekard0 <5880388+Rekard0@users.noreply.github.com>
2020-05-10 20:24:53 +0200

    adding enums & update methods (part 1)

--- blockchain/contracts/CarManager.sol
+++ blockchain/contracts/CarManager.sol
@@ -6,16 +6,33 @@ contract CarManager {
 
     uint256 ID;
 
-    mapping(address => bool) public brands;
+    mapping(address => bool) public brands; // now is dealer ship
 
     mapping(uint256 => Car) trackedCars;
 
-    mapping(address => bool) auhtorizedShops;
+    mapping(address => bool) auhtorizedShops; // now is mechanic
+
+    mapping(address => bool) public auhtorizedITV;
+
+    // Enums
+    enum CarType {Vehicle2W, Vehicle3W, Vehicle, VehicleH, VehicleA, VehicleS} // (!!) W, weel, H, Heavy S, Service A, Agriculture
+    enum CarOwnershipType {Factory, Brand, User}
+    enum CarState { Shiped, ForSale, ProcessingSale, Sold, ProcessingRgister, Registered} // processingSale ~ reserved
+    enum ITV {Initial, Passed, NotPassed, Negative} // NotPassed = car can go mechanic, Negative = car need transport to mechanic
+    // end - Enums
 
     struct Car {
         uint256 ID;
         address ownerID;
         string licensePlate;
+        // creation time perhaps needed
+        CarType carType;
+        // car state
+        CarOwnershipType carOwnerType;
+        CarState carState;
+        // ITV state
+        ITV itv;
+        // car history = perhaps it need a struct type!!
     }
 
     event CarAdded(uint256 ID);
@@ -29,6 +46,14 @@ contract CarManager {
         require(
             brands[msg.sender] == true,
             "Only active brand can add a new car"
+        );
+        _;
+    }
+
+    modifier onlyITV() {
+        require(
+            auhtorizedITV[msg.sender] == true,
+            "Only ITV can perform this action"
         );
         _;
     }
@@ -47,32 +72,60 @@ contract CarManager {
         brands[brandID] = true;
     }
 
+    function addITV(address ItvID) public onlyRoot {
+        auhtorizedITV[ItvID] = true;
+    }
+
     function deleteBrand(address brandID) public onlyRoot {
         brands[brandID] = false;
     }
 
-    function addCar(string memory licensePlate) public onlyActiveBrand {
-        ID = ID + 1;
+    function deleteITV(address ItvID) public onlyRoot {
+        auhtorizedITV[ItvID] = false;
+    }
 
+    function addCar(string memory licensePlate, uint256 _carTypeIndex) public onlyActiveBrand {
+        ID = ID + 1; // safe math !!
         trackedCars[ID] = Car({
             ID: ID,
             ownerID: msg.sender,
-            licensePlate: licensePlate
+            licensePlate: licensePlate,
+            // creation time perhaps needed
+            // setting default enums
+            // (!!) CarType can not be updated ??
+            carType: CarType(_carTypeIndex),
+            carOwnerType: CarOwnershipType.Brand,
+            carState: CarState.ForSale,
+            itv: ITV.Initial
         });
-
         emit CarAdded(ID);
     }
+
+    // update car
+    // update status
+    function updateCarOwenerType(uint256 _ID, uint256 _carOwnershipIndex) public onlyActiveBrand {
+        trackedCars[_ID].carOwnerType = CarOwnershipType(_carOwnershipIndex);
+    }
+    function updateCarState(uint256 _ID, uint256 _carStateIndex) public onlyActiveBrand { // modifier ??
+        trackedCars[_ID].carState = CarState(_carStateIndex);
+    }
+    function updateITV(uint256 _ID, uint256 _itv) public onlyITV {
+        trackedCars[_ID].itv = ITV(_itv);
+    }
+    // transfer, maintenance, ITV
 
     function getCar(uint256 _ID)
         public
         view
-        returns (uint256 carID, address ownerID, string memory licensePlate)
+        returns (uint256 carID, address ownerID, string memory licensePlate, uint256 carOwnerType, uint256 carITVstate)
     {
         Car memory car = trackedCars[_ID];
         return (
             carID = car.ID,
             ownerID = car.ownerID,
-            licensePlate = car.licensePlate
+            licensePlate = car.licensePlate,
+            carOwnerType = uint256(car.carOwnerType),
+            carITVstate = uint256(car.itv)
         );
     }
 }
--- blockchain/test/CarManager.js
+++ blockchain/test/CarManager.js
@@ -1,17 +1,18 @@
 const CarManager = artifacts.require("CarManager")
 
 contract("CarManager", accounts => {
+  let carManager
+
+  beforeEach(async () => {
+    carManager = await CarManager.deployed();
+  });
 
   it("sets deploying account as root", async () => {
-    const carManager = await CarManager.deployed();
-
     const rootAccount = await carManager.root.call();
     assert.equal(rootAccount, accounts[0], "Error: invalid root account");
   });
 
   it("fails to add a new brand as non-root account", async () => {
-    const carManager = await CarManager.deployed();
-
     try {
       await carManager.addBrand(accounts[1], { from: accounts[2] });
     } catch (e) {
@@ -20,17 +21,13 @@ contract("CarManager", accounts => {
   });
 
   it("adds a new brand as root account", async () => {
-    const carManager = await CarManager.deployed();
-
     await carManager.addBrand(accounts[1], { from: accounts[0] });
     const brand = await carManager.brands.call(accounts[1]);
     assert.equal(brand, true, "Error: failed to add a new brand from root account");
   });
 
-  it("fails to delete a brand from as non-root account", async () => {
-    const carManager = await CarManager.deployed();
+  it("fails to delete a brand from as non-root account", async () => {    
     await carManager.addBrand(accounts[1], { from: accounts[0] });
-
     try {
       await carManager.deleteBrand(accounts[1], { from: accounts[2] });
     } catch (e) {
@@ -39,8 +36,6 @@ contract("CarManager", accounts => {
   });
 
   it("deletes a brand as root account", async () => {
-    const carManager = await CarManager.deployed();
-
     await carManager.addBrand(accounts[1], { from: accounts[0] });
     await carManager.deleteBrand(accounts[1], { from: accounts[0] });
 
@@ -49,24 +44,57 @@ contract("CarManager", accounts => {
   });
 
   it("fails to add a new car as non-active brand", async () => {
-    const carManager = await CarManager.deployed();
-
     await carManager.addBrand(accounts[1], { from: accounts[0] });
     try {
-      await carManager.addCar("7610JBB", { from: accounts[0] });
+      await carManager.addCar("7610JBB", 2, { from: accounts[0] });
     } catch (e) {
       assert.equal(e.reason, "Only active brand can add a new car", "Error: invalid error reason");
     }
   });
 
   it("adds a new car as an active brand", async () => {
-    const carManager = await CarManager.deployed();
-
     await carManager.addBrand(accounts[1], { from: accounts[0] });
-    await carManager.addCar("7610JBB", { from: accounts[1] });
+    await carManager.addCar("7610JBB", 2, { from: accounts[1] });
 
     const { ownerID, licensePlate } = await carManager.getCar.call(1);
     assert.equal(ownerID, accounts[1], "Error: invalid ownerID");
     assert.equal(licensePlate, "7610JBB", "Error: invalid license plate");
+  });
+
+  it("Update Car Owner Type", async () => {
+    await carManager.addBrand(accounts[1], { from: accounts[0] });
+    await carManager.addCar("7610JBB", 2, { from: accounts[1] });
+    const { carOwnerType } = await carManager.getCar.call(1);
+    // console.log("oldtype = " + carOwnerType)
+    const old_carOwnerType = carOwnerType;
+    await carManager.updateCarOwenerType(1, 2, { from: accounts[1] });
+    const response = await carManager.getCar.call(1);
+    // console.log("newType = " + response.carOwnerType)
+    assert.notDeepEqual(old_carOwnerType, response.carOwnerType, "Error: carOwnerType not updated");
+  });
+  
+  it("adds a new ITV as root account", async () => {
+    await carManager.addITV(accounts[1], { from: accounts[0] });
+    const ITV = await carManager.auhtorizedITV.call(accounts[1]);
+    assert.equal(ITV, true, "Error: failed to add a new brand from root account");
+  });
+
+  it("deletes a ITV as root account", async () => {
+    await carManager.addITV(accounts[1], { from: accounts[0] });
+    await carManager.deleteITV(accounts[1], { from: accounts[0] });
+
+    const ITV = await carManager.auhtorizedITV.call(accounts[1]);
+    assert.equal(ITV, false, "Error: failed to delete brand from root account");
+  });
+
+  it("Update Car ITV state", async () => {
+    await carManager.addBrand(accounts[1], { from: accounts[0] });
+    await carManager.addCar("7610JBB", 2, { from: accounts[1] });
+    await carManager.addITV(accounts[2], { from: accounts[0] });
+    const { carITVstate } = await carManager.getCar.call(1);
+    const old_carITVstate = carITVstate;
+    await carManager.updateITV(1, 1, { from: accounts[2] });
+    const response = await carManager.getCar.call(1);
+    assert.notDeepEqual(old_carITVstate, response.carITVstate, "Error: carITVstate not updated");
   });
 });
