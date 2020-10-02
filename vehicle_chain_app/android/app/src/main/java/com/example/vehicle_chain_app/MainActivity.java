2cbc4c2b4de8186f299c4b01c67a80c885045647
Rekard0 <5880388+Rekard0@users.noreply.github.com>
2020-10-02 05:36:47 +0200

    reading raw pub key of CashApplet

--- vehicle_chain_app/android/app/src/main/java/com/example/vehicle_chain_app/MainActivity.java
+++ vehicle_chain_app/android/app/src/main/java/com/example/vehicle_chain_app/MainActivity.java
@@ -25,9 +25,94 @@ import im.status.keycard.applet.*;
 import org.bouncycastle.util.encoders.Hex;
 
 import android.os.Handler;
+import android.os.Looper;
 import android.util.Log;
 
 import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+class MyStreamHandler implements EventChannel.StreamHandler {
+    private static final String TAG = "MyStreamHandler";
+    private NFCCardManager cardManager;
+    private NfcAdapter nfcAdapter;
+    private FlutterActivity activity;
+
+    public MyStreamHandler(NFCCardManager iCardManager, NfcAdapter iNfcAdapter, FlutterActivity iActivity) {
+        cardManager = iCardManager;
+        nfcAdapter = iNfcAdapter;
+        activity = iActivity;
+    }
+
+    String response;
+    private final Handler handler = new Handler(Looper.getMainLooper());
+    private final Runnable runnable = new Runnable() {
+        @Override
+        public void run() {
+            cardManager.setCardListener(new CardListener() {
+                @Override
+                public void onDisconnected() {
+                    Log.i(TAG, "Card disconnected.");
+                    activity.runOnUiThread(() -> {
+                        eventSink.endOfStream();
+                    });
+                }
+                @Override
+                public void onConnected(CardChannel cardChannel) {
+                    try {
+                        // Applet-specific code we only use CashApplet
+                        CashCommandSet cmdSet = new CashCommandSet(cardChannel);
+
+                        Log.i(TAG, "Applet selection successful");
+
+                        // First thing to do is selecting the applet on the card.
+                        CashApplicationInfo info = new CashApplicationInfo(cmdSet.select().checkOK().getData());
+
+                        Log.i(TAG, "Applet PubKey: " + Arrays.toString(info.getPubKey()));
+                        Log.i(TAG, "Applet PubKey hex: " + Hex.toHexString(info.getPubKey()));
+                        Log.i(TAG, "Applet Version: " + info.getAppVersionString());
+                        Log.i(TAG, "Applet GetPubData: " + Hex.toHexString(info.getPubData()));
+
+                        response = Hex.toHexString(info.getPubKey());
+
+                        activity.runOnUiThread(() -> {
+                            eventSink.success(response);
+                        });
+                    } catch (Exception e) {
+                        activity.runOnUiThread(() -> {
+                            eventSink.error("exception happened", e.getMessage(), e.getCause());
+                        });
+                    }
+                }
+            });
+
+            handler.post(this);
+
+        }
+    };
+
+    private EventChannel.EventSink eventSink;
+
+    @Override
+    public void onListen(Object o, final EventChannel.EventSink eventSink) {
+        Log.i(TAG, "Listening...");
+        this.eventSink = eventSink;
+        runnable.run();
+        cardManager.start();
+        nfcAdapter.enableReaderMode(activity, cardManager, NfcAdapter.FLAG_READER_NFC_A | NfcAdapter.FLAG_READER_SKIP_NDEF_CHECK, null);
+    }
+
+    @Override
+    public void onCancel(Object o) {
+        Log.i(TAG, "Cancelling...");
+        if (nfcAdapter != null) {
+            nfcAdapter.disableReaderMode(activity);
+        }
+        handler.removeCallbacks(runnable);
+    }
+}
 
 public class MainActivity extends FlutterActivity {
     FlutterActivity activity = this;
@@ -38,63 +123,7 @@ public class MainActivity extends FlutterActivity {
     private NFCCardManager cardManager;
 
     private static final String METHODCHANNEL = "samples.flutter.dev/keycard";
-    private static final String EVENTCHANNEL = "samples.flutter.dev/keycardEevent";
-
-    // functions
-    String[] response = new String[4];
-
-    private void getKeycardApplicationInfo(final EventSink events) {
-
-        cardManager.setCardListener(new CardListener() {
-            @Override
-            public void onDisconnected() {
-                Log.i(TAG, "Card disconnected.");
-                events.endOfStream();
-            }
-            @Override
-            public void onConnected(CardChannel cardChannel) {
-                try {
-                    // Applet-specific code we only use CashApplet
-                    CashCommandSet cmdSet = new CashCommandSet(cardChannel);
-
-                    Log.i(TAG, "Applet selection successful");
-
-                    // First thing to do is selecting the applet on the card.
-                    ApplicationInfo info = new ApplicationInfo(cmdSet.select().checkOK().getData());
-
-                    // If the card is not initialized, the INIT apdu must be sent. The actual PIN, PUK and pairing password values
-                    // can be either generated or chosen by the user. Using fixed values is highly discouraged.
-                    // if (!info.isInitializedCard()) {
-                    //     Log.i(TAG, "Initializing card with test secrets");
-                    //     cmdSet.init("000000", "123456789012", "KeycardTest").checkOK();
-                    //     info = new ApplicationInfo(cmdSet.select().checkOK().getData());
-                    // }
-
-                    Log.i(TAG, "Instance UID: " + Hex.toHexString(info.getInstanceUID()));
-                    response[0] = new String("Instance UID: " + Hex.toHexString(info.getInstanceUID()));
-                    Log.i(TAG, "Secure channel public key: " + Hex.toHexString(info.getSecureChannelPubKey()));
-                    response[1] = Hex.toHexString(info.getSecureChannelPubKey());
-                    Log.i(TAG, "Application version: " + info.getAppVersionString());
-                    response[2] = info.getAppVersionString();
-
-                    // if (info.hasMasterKey()) {
-                    //     Log.i(TAG, "Key UID: " + Hex.toHexString(info.getKeyUID()));
-                    //     response[4] = Hex.toHexString(info.getKeyUID());
-                    // } else {
-                    //     Log.i(TAG, "The card has no master key");
-                    //     response[4] = "The card has no master key";
-                    // }
-                    response[3] = "all read success";
-                    events.success(response);
-                } catch (Exception e) {
-                    events.error("failed to listen", e.getMessage(), e.getCause());
-                }
-            }
-        });
-
-        cardManager.start();
-
-    }
+    private static final String EVENTCHANNEL = "samples.flutter.dev/getCardPubKeyEevent";
 
 
     @Override
@@ -118,29 +147,12 @@ public class MainActivity extends FlutterActivity {
                     }
                 }
         );
-        Handler uiThreadHandler = new Handler(activity.getMainLooper());
-        uiThreadHandler.post(new Runnable() {
-            @Override
-            public void run() {
-                new EventChannel(flutterEngine.getDartExecutor(), EVENTCHANNEL).setStreamHandler(
-                        new StreamHandler() {
-                            @Override
-                            public void onListen(Object arguments, EventSink events) {
-                                getKeycardApplicationInfo(events);
-                                nfcAdapter.enableReaderMode(activity, cardManager, NfcAdapter.FLAG_READER_NFC_A | NfcAdapter.FLAG_READER_SKIP_NDEF_CHECK, null);
 
-                            }
-
-                            @Override
-                            public void onCancel(Object arguments) {
-                                if (nfcAdapter != null) {
-                                    nfcAdapter.disableReaderMode(activity);
-                                }
-                            }
-                        }
-                );
-            }});
-
+        Log.i(TAG, "Creating event channel...");
+        new EventChannel(flutterEngine.getDartExecutor(), EVENTCHANNEL).setStreamHandler(
+                new MyStreamHandler(cardManager, nfcAdapter, activity)
+        );
 
     }
 }
+
--- vehicle_chain_app/lib/screens/nfcTester.dart
+++ vehicle_chain_app/lib/screens/nfcTester.dart
@@ -10,9 +10,11 @@ class NfcTester extends StatefulWidget {
 
 class _NfcTesterState extends State<NfcTester> {
   static const platformMethods = const MethodChannel('samples.flutter.dev/keycard');
-  static const platformEvents = const EventChannel('samples.flutter.dev/keycardEevent');
+  static const platformEvents = const EventChannel('samples.flutter.dev/getCardPubKeyEevent');
+
   String _isNfcAdapterEnabled = 'Unknown Nfc Status Yet.';
-  List<String> _nfcApplicationInfo;
+  String _nfcCardPubKeyError = 'no error yet';
+  String _nfcCardPubKey;
 
   Future<void> _getNfcAdapterStatus() async {
     String isNfcAdapterEnabled;
@@ -35,15 +37,18 @@ class _NfcTesterState extends State<NfcTester> {
   }
 
   void _onEvent(Object event) {
-    log(event);
-    // setState(() {
-    //   _nfcApplicationInfo = event;
-    // });
+    log(event.toString());
+    log(event.toString().length.toString());
+
+    setState(() {
+      _nfcCardPubKey = event;
+    });
   }
 
   void _onError(Object error) {
+    log(error.toString());
     setState(() {
-      _nfcApplicationInfo = [error.toString()];
+      _nfcCardPubKeyError = error.toString();
     });
   }
 
@@ -53,31 +58,27 @@ class _NfcTesterState extends State<NfcTester> {
       appBar: AppBar(
         title: Text('Nfc Testing'),
       ),
-      body: Column(
-        children: [
-          SizedBox(height: 20.0),
-          RaisedButton(
-            child: Text('Call Nfc adapter'),
-            onPressed: () async {
-              await _getNfcAdapterStatus();
-            },
-          ),
-          SizedBox(height: 20.0),
-          Center(
-            child: Text(_isNfcAdapterEnabled),
-          ),
-          // SizedBox(height: 20.0),
-          // RaisedButton(
-          //   child: Text('Call Keycard Info'),
-          //   onPressed: () async {
-          //     await _getCashApplicationInfo();
-          //   },
-          // ),
-          SizedBox(height: 20.0),
-          Center(
-            child: Text(_nfcApplicationInfo.toString()),
-          ),
-        ],
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            SizedBox(height: 20.0),
+            RaisedButton(
+              child: Text('Call Nfc adapter'),
+              onPressed: () async {
+                await _getNfcAdapterStatus();
+              },
+            ),
+            SizedBox(height: 20.0),
+            Center(
+              child: Text(_isNfcAdapterEnabled),
+            ),
+            SizedBox(height: 20.0),
+            Center(
+              child: Text(_nfcCardPubKey ?? _nfcCardPubKeyError),
+            ),
+          ],
+        ),
       ),
     );
   }
