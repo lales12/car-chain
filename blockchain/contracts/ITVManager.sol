226802e36dc240a8b872939d7ff5b562a154fac0
Rekard0 <5880388+Rekard0@users.noreply.github.com>
2020-11-05 15:23:25 +0100

    update itv in blockchain

--- blockchain/contracts/ITVManager.sol
+++ blockchain/contracts/ITVManager.sol
@@ -2,9 +2,12 @@ pragma solidity >=0.4.21 <0.7.0;
 
 import "./interfaces/CarInterface.sol";
 import "./BaseManager.sol";
+import { ECDSA } from  "./libraries/ECDSA.sol";
 
 
 contract ITVManager  is BaseManager {
+    using ECDSA for bytes32;
+
     string constant UPDATE_METHOD = 'updateITV(uint256,uint256)';
 
     enum ITVState {PASSED, NOT_PASSED, NEGATIVE}
@@ -14,24 +17,48 @@ contract ITVManager  is BaseManager {
         uint256 date;
     }
 
-    mapping(uint256 => ITVInspection[]) ITVs;
+    mapping(address => ITVInspection[]) ITVs;
 
-    event ITVInspectionEvent(uint256 carID, ITVState state);
+    event ITVInspectionEvent(address carAddress, ITVState state);
 
     constructor(
         address authorizerContractAddress,
         address carAssetContractAddress
     ) BaseManager(authorizerContractAddress, carAssetContractAddress) public {}
 
-    function updateITV(uint256 carId, ITVState itvStateIndex) 
+    function updateITV(
+        bytes32 carIdHash,
+        bytes memory signature,
+        ITVState itvStateIndex
+    ) 
         public
         onlyAuthorized (UPDATE_METHOD, msg.sender)
     {
-        ITVs[carId].push(
+        address carAddress = carIdHash.recover(signature);
+
+        require(carAddress != address(0), "car address not found.");
+
+        ITVs[carAddress].push(
             ITVInspection(itvStateIndex, now)
         );
         
-        emit ITVInspectionEvent(carId, itvStateIndex);
+        emit ITVInspectionEvent(carAddress, itvStateIndex);
     }
+
+    function getITVState(
+        address carAddress
+    )
+        public 
+        view
+        returns(ITVState state, uint256 date)
+    {
+        uint256 itvLength = ITVs[carAddress].length;
+
+        require(itvLength > 0, "This car don't have inspection");
+        
+        state = ITVs[carAddress][itvLength].state;
+        date = ITVs[carAddress][itvLength].date;
+        
+    } 
 
 }
