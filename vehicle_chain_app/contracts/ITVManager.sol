f2a075e0778987b407dd881077c33cf0046c1ccc
lales120 <alexflores120@gmail.com>
2020-11-05 10:53:04 +0100

    Add itv contract

--- vehicle_chain_app/contracts/ITVManager.sol
+++ vehicle_chain_app/contracts/ITVManager.sol
@@ -1,10 +1,15 @@
 pragma solidity >=0.4.21 <0.7.0;
 
+import { ECDSA } from  "./libraries/ECDSA.sol";
+
+
 import "./interfaces/CarInterface.sol";
 import "./BaseManager.sol";
 
 
 contract ITVManager  is BaseManager {
+    using ECDSA for bytes32;
+
     string constant UPDATE_METHOD = 'updateITV(uint256,uint256)';
 
     enum ITVState {PASSED, NOT_PASSED, NEGATIVE}
@@ -14,24 +19,48 @@ contract ITVManager  is BaseManager {
         uint256 date;
     }
 
-    mapping(uint256 => ITVInspection[]) ITVs;
+    mapping(address => ITVInspection[]) ITVs;
 
-    event ITVInspectionEvent(uint256 carID, ITVState state);
+    event ITVInspectionEvent(address carAddress, ITVState state);
 
     constructor(
         address authorizerContractAddress,
         address carAssetContractAddress
     ) BaseManager(authorizerContractAddress, carAssetContractAddress) public {}
 
-    function updateITV(uint256 carId, ITVState itvStateIndex) 
+    function updateITV(
+        bytes32 carIdHash,
+        bytes calldata signature,
+        ITVState itvStateIndex
+    ) 
         public
         onlyAuthorized (UPDATE_METHOD, msg.sender)
     {
-        ITVs[carId].push(
+        address carAddress = carIdHash.recover(signature);
+
+        require(carAddress != address(0), "car address not found.");
+
+        ITVs[carAddress].push(
             ITVInspection(itvStateIndex, now)
         );
         
-        emit ITVInspectionEvent(carId, itvStateIndex);
+        emit ITVInspectionEvent(carAddress, itvStateIndex);
     }
 
+
+    function getITVState(
+        address carAddress
+    )
+        public 
+        view
+        returns(ITVState state, uint256 date)
+    {
+        uint256 itvLength = ITVs[carAddress].length;
+
+        require(itvLength, "This car don't have inspection")
+        return (
+            ITVs[carAddress][itvLength].state,
+            ITVs[carAddress][itvLength].date,
+        )
+    } 
 }
